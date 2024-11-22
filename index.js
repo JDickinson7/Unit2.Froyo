eb93d95090d7ec7fd4aae9aecb7304a0fc6d2adb
Jessica Dickinson <your_email@example.com>
2024-11-21 21:50:05 -0700

    added the skeleton to js

---
+++ index.js
@@ -0,0 +1,16 @@
+//Create a prompt funtion that alllows the customer to put in the flavors they prefer
+prompt ="What flavors would you like today? We have Vanilla, Strawberry, and Coffee! Please comma seperate your order!"
+
+//create an array from the input/strings prompt 
+
+
+//create a loop to iterate through the array of flavors? 
+
+
+//create an object const to keep count of how many orders of each flavor there are
+
+
+//correctly count the number of times a flavor in the users input
+
+
+///CHECK! logic for the array is organized, names are consistent, and the console changes depending on the input
